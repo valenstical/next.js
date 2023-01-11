f1296716e4daf42f96cbb327f0d2e19b542c4d35
Valentine <valentine@menskin.co>
2023-01-11 15:15:07 +0100

    initial test

--- pages/api/verify-account.js
+++ pages/api/verify-account.js
@@ -1,6 +1,6 @@
 import Cors from "cors";
 
-// Initializing the cors middleware
+// Initializing the cors middleware,
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
   methods: ["POST", "GET", "HEAD"],
